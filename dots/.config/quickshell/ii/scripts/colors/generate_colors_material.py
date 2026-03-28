1d44de1b41a3274c1afc009f4a3d4d3c702c837d
vaguesyntax <talhaozturak@gmail.com>
2026-03-28 15:36:10 +0300

    color gen: fix for matugen >= 4.0

--- dots/.config/quickshell/ii/scripts/colors/generate_colors_material.py
+++ dots/.config/quickshell/ii/scripts/colors/generate_colors_material.py
@@ -137,7 +137,7 @@ if args.termscheme is not None:
         json_termscheme = f.read()
     term_source_colors = json.loads(json_termscheme)['dark' if darkmode else 'light']
 
-    primary_color_argb = hex_to_argb(material_colors['primary_paletteKeyColor'])
+    primary_color_argb = hex_to_argb(material_colors['primaryPaletteKeyColor'])
     for color, val in term_source_colors.items():
         if(args.scheme == 'monochrome') :
             term_colors[color] = val
