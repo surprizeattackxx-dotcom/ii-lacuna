aad049e1714db419ae7d4f7f8a7a9a1eab2d35a6
vaguesyntax <talhaozturak@gmail.com>
2026-03-31 20:04:25 +0300

    generate color materials: use a fallback color to prevent script crash

--- dots/.config/quickshell/ii/scripts/colors/generate_colors_material.py
+++ dots/.config/quickshell/ii/scripts/colors/generate_colors_material.py
@@ -137,8 +137,13 @@ if args.termscheme is not None:
         json_termscheme = f.read()
     term_source_colors = json.loads(json_termscheme)['dark' if darkmode else 'light']
 
-    primary_color_argb = hex_to_argb(material_colors['primary_paletteKeyColor'])
-    for color, val in term_source_colors.items():
+    try: 
+        primary_color_argb = hex_to_argb(material_colors['primary_paletteKeyColor'])
+    except KeyError:
+        print("primary_paletteKeyColor not found in material colors, using white as fallback")
+        primary_color_argb = hex_to_argb("#ffffff")
+    
+for color, val in term_source_colors.items():
         if(args.scheme == 'monochrome') :
             term_colors[color] = val
             continue
